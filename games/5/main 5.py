67038b8f975e5148c890332db3217cc1ec25af94
greenem-official <vanyapopov2004@gmail.com>
2023-11-10 21:33:42 +0300

    added task 5

--- games/5/main 5.py
+++ games/5/main 5.py
@@ -1,7 +1,7 @@
-from random import *
-
-a = []
-first = True
+b = []
+player = 1
+sizeX = 5
+sizeY = 4
 
 def toStr(num):
     if num < 10:
@@ -10,89 +10,149 @@ def toStr(num):
         return str(num)
 
 def draw():
-    for i in range(len(a)):
+
+    print('   X ', end='')
+    for i in range(len(b[0])):
+        print((i + 1), '', end='')
+    print()
+    print(' Y   ', (len(b[0])) * '— ', sep='')
+    for i in range(len(b)):
         print(toStr(i+1), ' | ', sep='', end='')
-        for j in range(len(a[i])):
-            if a[i][j]:
-                print('o ', end='')
+        for j in range(len(b[i])):
+            if b[i][j] == '':
+                print('0 ', end='')
             else:
-                print('_ ', end='')
+                print(str(b[i][j]) + ' ', end='')
         print()
-    print('     ', (len(a) * 2) * '—', sep='')
-    print('     ', end='')
-    for i in range(len(a)):
-        print((i+1), '', end='')
     print()
 
-def remove(t, x):
-    hadAny = False
-    if t == 'г':
-        i = x
-        for j in range(len(a)):
-            if a[i][j]:
-                hadAny = True
-            a[i][j] = False
-    elif t == 'в':
-        j = x
-        for i in range(len(a)):
-            if a[i][j]:
-                hadAny = True
-            a[i][j] = False
-    return hadAny
+# def draw():
+#     for i in range(len(b)):
+#         print(toStr(i+1), ' | ', sep='', end='')
+#         for j in range(len(b[i])):
+#             if b[i][j] == '':
+#                 print('_ ', end='')
+#             else:
+#                 print(str(b[i][j]) + ' ', end='')
+#         print()
+#     print(' Y   ', (len(b[0]) * 2 - 1) * '—', sep='')
+#     print('   X ', end='')
+#     for i in range(len(b[0])):
+#         print((i+1), '', end='')
+#     print()
+
+def setElem(x, y):
+    if b[x][y] == '':
+        b[x][y] = player
+        return True
+    else:
+        return False
+
+def getNearAmount(x, y, playerNum):
+    c = 0
+    if x > 0 and y > 0: # bottom left
+        if b[y - 1][x - 1] == playerNum:
+            c += 1
+    if x > 0: # left
+        if b[y][x - 1] == playerNum:
+            c += 1
+    if x > 0 and y < sizeY-1: # top left
+        if b[y+1][x-1] == playerNum:
+            c += 1
+    if y > 0: # bottom
+        if b[y-1][x] == playerNum:
+            c += 1
+    if y < sizeY-1: # top
+        if b[y+1][x] == playerNum:
+            c += 1
+    if x < sizeX-1 and y < sizeY-1: # top right
+        if b[y + 1][x + 1] == playerNum:
+            c += 1
+    if x < sizeX-1: # right
+        if b[y][x + 1] == playerNum:
+            c += 1
+    if x < sizeX-1 and y > 0: # bottom right
+        if b[y-1][x+1] == playerNum:
+            c += 1
+    return c
+
+def toActualPoints(n):
+    return max(0, n // 2 - 1)
+
+def decideResults(arr):
+    if arr[1] < arr[2] and arr[1] < arr[3]:
+        print('Первый игрок победил!')
+    elif arr[2] < arr[1] and arr[2] < arr[3]:
+        print('Второй игрок победил!')
+    elif arr[3] < arr[1] and arr[3] < arr[2]:
+        print('Третий игрок победил!')
+    else:
+        print('Нет единого победителя')
 
 def validate():
-    global first
-    hasAny = False
-    for i in range(len(a)):
-        if not hasAny:
-            for j in range(len(a[i])):
-                if not hasAny:
-                    if a[i][j]:
-                        hasAny = True
+    done = True
+    for i in range(len(b)):
+        for j in range(len(b[i])):
+            if b[i][j] == '':
+                pass
+                return False
 
-    if not hasAny:
-        if first:
-            print('Первый игрок победил!')
-            return True
-        else:
-            print('Второй игрок победил!')
-            return True
-    return False
+    minusPoints = {}
+    for p in range(1, 4):
+        counter = 0
+        for i in range(len(b)):
+            for j in range(len(b[i])):
+                if b[i][j] == p:
+                    counter += getNearAmount(j, i, p)
+        minusPoints[p] = counter
+        counter = 0
+
+    # print(minusPoints)
+    print('Штрафные очки: ', toActualPoints(minusPoints[1]), ', ', toActualPoints(minusPoints[2]), ', ', toActualPoints(minusPoints[3]), sep='')
+
+    decideResults(minusPoints)
+    return True
+
+def playerNumText():
+    plText = 'Первый'
+    if player == 2:
+        plText = 'Второй'
+    elif player == 3:
+        plText = 'Третий'
+    return plText
 
 if __name__ == '__main__':
     size = 10
     chance = 4
-    for i in range(size):
-        b = []
-        for j in range(size):
-            r = randint(0, chance-1)
-            b.append(r == 0)
-        a.append(b)
+    for i in range(sizeY):
+        c = []
+        for j in range(sizeX):
+            c.append('')
+        b.append(c)
 
     draw()
-    # global first
     while True:
         try:
-            if first:
-                t, x, = input('Первый игрок: Введите тип (горизонталь - г, вертикаль - в) и номер: ').split()
-            else:
-                t, x = input('Второй игрок: Введите тип (горизонталь - г, вертикаль - в) и номер: ').split()
-            if not x.isdigit():
-                print("Ввод должен быть двумя числом!")
-                continue
-            if t != 'г' and t != 'в':
-                print('Тип должен быть "г" (горизонталь) или "в" (вертикаль)!')
+            y, x = input(playerNumText() + ' игрок: Введите координаты x и y: ').split()
+            if not x.isdigit() or not y.isdigit():
+                print("Координаты должны быть двумя числами!")
                 continue
             x = int(x)
-            if x < 1 or x > size:
-                print("Номер должен быть в диапазоне 1-", size, "!", sep='')
+            y = int(y)
+            if x < 1 or x > 4 or y < 1 or y > 5:
+                print("Номер должен быть в диапазоне 1-5 и 1-4!", sep='')
                 continue
-            if not remove(t, x-1):
-                print("Данная линия пуста!")
+            if not setElem(x-1, y-1):
+                print("Данная ячейка уже занята!")
                 continue
             draw()
             if validate():
                 break
-            first = not first
+
+            player += 1
+            if player == 4:
+                player = 1
         except ValueError:
-            print("Ввод должен быть двумя числами!")
+            print("Координаты должны быть двумя числами!")
+        except KeyboardInterrupt:
+            validate()
--- seminars/sem 3/7.py
+++
@@ -1,7 +0,0 @@
-x = 'First value'
-y = 'Second value'
-
-a = []
-
-friend = x if len(a) == 0 else y
-print(friend)
